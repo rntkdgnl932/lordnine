d04aa9618d494e9ac56d220bf88efade754f9d6d
coob <koosanghwi@gmail.com>
2024-07-13 13:28:22 +0900

    ver 1.032

--- data_lordnine/mymodule/stop_event18.py
+++ data_lordnine/mymodule/stop_event18.py
@@ -45,4 +45,52 @@ def _stop_please(cla):
         return 0
 
 
+def game_check(cla):
+    import numpy as np
+    import cv2
+    import os
+
+    from function_game import imgs_set_
+    from massenger import line_to_me
+
+    try:
+        print("game_check")
+
+        why = "none"
+
+        is_error = False
+
+        full_path = "c:\\my_games\\lordnine\\data_lordnine\\imgs\\check\\etc_check\\network_error.PNG"
+        img_array = np.fromfile(full_path, np.uint8)
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        imgs_ = imgs_set_(70, 670, 300, 100, cla, img, 0.7)
+        if imgs_ is not None and imgs_ != False:
+            print("network_error")
+            is_error = True
+            why = str(v_.this_game) + "네트워크 에러"
+
+        if is_error == True:
+
+            print(why)
+            line_to_me(v_.now_cla, why)
+
+            dir_path = "C:\\my_games\\load\\" + str(v_.game_folder)
+            file_path = dir_path + "\\start.txt"
+            # cla.txt
+            cla_data = str(v_.now_cla) + "cla"
+            file_path2 = dir_path + "\\" + cla_data + ".txt"
+            with open(file_path, "w", encoding='utf-8-sig') as file:
+                data = 'no'
+                file.write(str(data))
+                time.sleep(0.2)
+            with open(file_path2, "w", encoding='utf-8-sig') as file:
+                data = v_.now_cla
+                file.write(str(data))
+                time.sleep(0.2)
+            os.execl(sys.executable, sys.executable, *sys.argv)
+
+
+    except Exception as e:
+        print(e)
+        return 0
 
