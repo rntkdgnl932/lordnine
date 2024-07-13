a662b8a39dc8a73cc07195d89a41bd29b014ef7b
coob <koosanghwi@gmail.com>
2024-07-13 13:16:36 +0900

    ver 1.021

--- data_lordnine/mymodule/tuto_lordnine.py
+++ data_lordnine/mymodule/tuto_lordnine.py
@@ -268,7 +268,7 @@ def quest_checking(cla):
         full_path = "c:\\my_games\\lordnine\\data_lordnine\\imgs\\tuto\\ganghwa_btn.PNG"
         img_array = np.fromfile(full_path, np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        imgs_ = imgs_set_(300, 670, 400, 730, cla, img, 0.8)
+        imgs_ = imgs_set_(300, 670, 400, 730, cla, img, 0.85)
         if imgs_ is not None and imgs_ != False:
             print("ganghwa_btn : 1차 튜토 끝", imgs_)
             why = str(v_.this_game) + " 1차 튜토 끝"
