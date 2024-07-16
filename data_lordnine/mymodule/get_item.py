07b77fa063d8d88e6384f294299744fba2256daa
coob <koosanghwi@gmail.com>
2024-07-16 15:41:16 +0900

    ver 1.103

--- data_lordnine/mymodule/get_item.py
+++ data_lordnine/mymodule/get_item.py
@@ -766,6 +766,33 @@ def get_post(cla):
 
                 post = True
 
+                for i in range(10):
+                    full_path = "c:\\my_games\\lordnine\\data_lordnine\\imgs\\get_item\\menu_point_1.PNG"
+                    img_array = np.fromfile(full_path, np.uint8)
+                    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                    imgs_ = imgs_set_(50, 50, 610, 100, cla, img, 0.65)
+                    if imgs_ is not None and imgs_ != False:
+                        print("menu_point_1", imgs_)
+                        break
+
+                    else:
+                        full_path = "c:\\my_games\\lordnine\\data_lordnine\\imgs\\get_item\\menu_point_2.PNG"
+                        img_array = np.fromfile(full_path, np.uint8)
+                        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                        imgs_ = imgs_set_(50, 50, 610, 100, cla, img, 0.65)
+                        if imgs_ is not None and imgs_ != False:
+                            print("menu_point_2", imgs_)
+                            break
+
+                        else:
+                            full_path = "c:\\my_games\\lordnine\\data_lordnine\\imgs\\get_item\\post_point_1.PNG"
+                            img_array = np.fromfile(full_path, np.uint8)
+                            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                            imgs_ = imgs_set_(50, 50, 610, 100, cla, img, 0.65)
+                            if imgs_ is not None and imgs_ != False:
+                                print("post_point_1", imgs_)
+                                break
+                    time.sleep(0.5)
                 # 상단 포인트
                 for i in range(10):
 
