b51d0f5455ebe9853f008f28bc0fd0925b7c0c56
coob <koosanghwi@gmail.com>
2024-07-19 14:13:24 +0900

    ver 1.201

--- data_lordnine/mymodule/character_select_and_game_start.py
+++ data_lordnine/mymodule/character_select_and_game_start.py
@@ -304,6 +304,15 @@ def game_ready(cla):
             click_pos_reg(imgs_.x, imgs_.y, cla)
         else:
             # 완전 바깥일 경우 일딴 들어가기(touch_to_start)
+            full_path = "c:\\my_games\\lordnine\\data_lordnine\\imgs\\character_select_and_game_start\\lordnine_mark.PNG"
+            img_array = np.fromfile(full_path, np.uint8)
+            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+            imgs_ = imgs_set_(420, 820, 540, 940, cla, img, 0.8)
+            if imgs_ is not None and imgs_ != False:
+                confirm_all(cla)
+                time.sleep(0.5)
+
+
             full_path = "c:\\my_games\\lordnine\\data_lordnine\\imgs\\character_select_and_game_start\\touch_to_start.PNG"
             img_array = np.fromfile(full_path, np.uint8)
             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
