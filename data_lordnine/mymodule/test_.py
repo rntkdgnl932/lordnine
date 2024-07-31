06103408cd7841bef555472c2b65bee4fe26d39a
coob <koosanghwi@gmail.com>
2024-08-01 01:41:59 +0900

    ver 1.24902

--- data_lordnine/mymodule/test_.py
+++ data_lordnine/mymodule/test_.py
@@ -69,12 +69,12 @@ def go_test():
         # text_check_get(512, 315, 542, 360, cla)
         time.sleep(1)
         # drag_pos(400, 500, 850, 500, cla)
-        full_path = "c:\\my_games\\lordnine\\data_lordnine\\imgs\\check\\out\\talk.PNG"
+        full_path = "c:\\my_games\\lordnine\\data_lordnine\\imgs\\dead_die\\recorvery_time.PNG"
         img_array = np.fromfile(full_path, np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-        imgs_ = imgs_set_(0, 940, 60, 990, "three", img, 0.75)
+        imgs_ = imgs_set_(0, 30, 300, 400, "three", img, 0.75)
         if imgs_ is not None and imgs_ != False:
-            print("talk", imgs_)
+            print("recorvery_time", imgs_)
 
         # mission_get_daily(cla, data)
 
