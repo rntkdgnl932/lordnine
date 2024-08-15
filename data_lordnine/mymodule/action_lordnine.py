f8bcf8bc7c88acaa1821ea8ab02867160603e6ff
coob <koosanghwi@gmail.com>
2024-08-15 13:53:52 +0900

    ver 1.2872

--- data_lordnine/mymodule/action_lordnine.py
+++ data_lordnine/mymodule/action_lordnine.py
@@ -226,7 +226,7 @@ def out_check(cla):
                 full_path = "c:\\my_games\\lordnine\\data_lordnine\\imgs\\clean_screen\\close_3.PNG"
                 img_array = np.fromfile(full_path, np.uint8)
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(0, 100, 960, 1040, cla, img, 0.8)
+                imgs_ = imgs_set_(0, 100, 960, 1040, cla, img, 0.9)
                 if imgs_ is not None and imgs_ != False:
                     print("close_3 : 아직 안 꺼짐", imgs_)
                 else:
