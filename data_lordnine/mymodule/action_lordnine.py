612f0a1650427e18d514b80370661b0b2bae37e8
coob <koosanghwi@gmail.com>
2024-09-12 04:07:10 +0900

    ver 1.392

--- data_lordnine/mymodule/action_lordnine.py
+++ data_lordnine/mymodule/action_lordnine.py
@@ -932,8 +932,10 @@ def move_check(cla):
 def loading_check(cla):
     import numpy as np
     import cv2
+    import os
 
     from function_game import imgs_set_, click_pos_reg
+    from massenger import line_to_me
     try:
 
         print("loading_check")
@@ -974,6 +976,49 @@ def loading_check(cla):
             if imgs_ is not None and imgs_ != False:
                 print("로딩중", is_move_second, "초")
                 is_move_count = 0
+                if is_move_second > 200:
+                    print("이상하다.")
+                    full_path = "c:\\my_games\\lordnine\\data_lordnine\\imgs\\check\\no_response.PNG"
+                    img_array = np.fromfile(full_path, np.uint8)
+                    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                    imgs_ = imgs_set_(0, 0, 600, 30, cla, img, 0.8)
+                    if imgs_ is not None and imgs_ != False:
+
+                        no_response = True
+
+                        for i in range(60):
+                            full_path = "c:\\my_games\\lordnine\\data_lordnine\\imgs\\check\\no_response.PNG"
+                            img_array = np.fromfile(full_path, np.uint8)
+                            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                            imgs_ = imgs_set_(0, 0, 600, 30, cla, img, 0.8)
+                            if imgs_ is not None and imgs_ != False:
+                                print("no_response", imgs_)
+                            else:
+                                no_response = False
+                                break
+                            time.sleep(1)
+
+                        if no_response == True:
+                            print("알람하고 끄자")
+
+                            why = "로딩 200초 이상 후 응답없음 1분 이상..."
+                            print(why)
+                            line_to_me(cla, why)
+
+                            dir_path = "C:\\my_games\\load\\" + str(v_.game_folder)
+                            file_path = dir_path + "\\start.txt"
+                            # cla.txt
+                            cla_data = str(cla) + "cla"
+                            file_path2 = dir_path + "\\" + cla_data + ".txt"
+                            with open(file_path, "w", encoding='utf-8-sig') as file:
+                                data = 'no'
+                                file.write(str(data))
+                                time.sleep(0.2)
+                            with open(file_path2, "w", encoding='utf-8-sig') as file:
+                                data = cla
+                                file.write(str(data))
+                                time.sleep(0.2)
+                            os.execl(sys.executable, sys.executable, *sys.argv)
 
             else:
                 is_move_count += 1
