26412d1b35c81e9d0e7297e16f7695dd238ecd0b
coob <koosanghwi@gmail.com>
2024-11-02 09:07:21 +0900

    ver 1.471

--- data_lordnine/mymodule/jadong_lordnine.py
+++ data_lordnine/mymodule/jadong_lordnine.py
@@ -174,7 +174,7 @@ def spot_in_ready(cla, where):
                     full_path = "c:\\my_games\\lordnine\\data_lordnine\\imgs\\jadong\\big_map\\" + str(result_spot[1]) + ".PNG"
                     img_array = np.fromfile(full_path, np.uint8)
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    imgs_ = imgs_set_(20, 100, 120, 150, cla, img, 0.85)
+                    imgs_ = imgs_set_(20, 130, 120, 180, cla, img, 0.85)
                     if imgs_ is not None and imgs_ != False:
                         print("big_map", result_spot[1], imgs_)
 
@@ -398,7 +398,7 @@ def spot_in_ready(cla, where):
                                 result_spot[1]) + ".PNG"
                             img_array = np.fromfile(full_path, np.uint8)
                             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                            imgs_ = imgs_set_(20, 100, 120, 150, cla, img, 0.85)
+                            imgs_ = imgs_set_(20, 130, 120, 180, cla, img, 0.85)
                             if imgs_ is not None and imgs_ != False:
                                 break
                             else:
@@ -486,7 +486,7 @@ def spot_in(cla, where):
                     full_path = "c:\\my_games\\lordnine\\data_lordnine\\imgs\\jadong\\big_map\\" + str(result_spot[1]) + ".PNG"
                     img_array = np.fromfile(full_path, np.uint8)
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    imgs_ = imgs_set_(20, 100, 120, 150, cla, img, 0.7)
+                    imgs_ = imgs_set_(20, 130, 120, 180, cla, img, 0.7)
                     if imgs_ is not None and imgs_ != False:
                         print("big_map", result_spot[1], imgs_)
 
--- data_lordnine/mymodule/test_.py
+++ data_lordnine/mymodule/test_.py
@@ -63,7 +63,12 @@ def go_test():
         # where = "성전사단격전지/계승자초소"
         # jadong_start(cla, where)
 
-        get_gold_sohwan(cla)
+        full_path = "c:\\my_games\\lordnine\\data_lordnine\\imgs\\jadong\\big_map\\1.PNG"
+        img_array = np.fromfile(full_path, np.uint8)
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        imgs_ = imgs_set_(20, 130, 120, 180, cla, img, 0.7)
+        if imgs_ is not None and imgs_ != False:
+            print("ggggggggggggggggggggg")
 
         # for i in range(15):
         #     full_path = "c:\\my_games\\lordnine\\data_lordnine\\imgs\\boonhae_collection\\right_checked.PNG"
